84affa2265d6c1f02896b22899f307e2dfd0660c
jlmunozfdev <jlmunozf.dev@gmail.com>
2022-05-02 20:46:52 -0500

    Creamos fun_ope_basicas

---
+++ PYTHON/SESION_7/modulo1/ope_básicas.py
@@ -0,0 +1,11 @@
+def sumar(ope1, ope2):
+    print("La suma es:", ope1+ope2)
+
+def restar(ope1, ope2):
+    print("La resta es:", ope1-ope2)
+
+def multiplicar(ope1, ope2):
+    print("La multiplicación es:", ope1*ope2)
+
+def dividir(ope1, ope2):
+    print("La división es:", ope1/ope2)
