0997fcfff477ff301812a67db61bea80a947e4bb
jlmunozfdev <jlmunozf.dev@gmail.com>
2022-05-19 14:27:55 -0500

    Congiguramos las vistas en películas

--- PYTHON/SESION_12/DJANGO/peliculas/views.py
+++ PYTHON/SESION_12/DJANGO/peliculas/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Director, Pelicula
+
+class IndexView(generic.ListView):
+    template_name = 'peliculas/directores.html'
+    model = Director
+
+class DirectorView(generic.DetailView):
+    template_name = 'peliculas/director.html'
+    model = Director
+
+class PeliculaView(generic.DetailView):
+    template_name = 'peliculas/pelicula.html'
+    model = Pelicula
