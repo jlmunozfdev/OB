d1d06da2ef43932578ada72fef9ad5d6f4ef8b25
jlmunozfdev <jlmunozf.dev@gmail.com>
2022-05-19 14:51:16 -0500

    Mejoramos algunos ajustes

--- PYTHON/SESION_12/DJANGO/peliculas/views.py
+++ PYTHON/SESION_12/DJANGO/peliculas/views.py
@@ -3,13 +3,13 @@ from django.views import generic
 from .models import Director, Pelicula
 
 class IndexView(generic.ListView):
-    template_name = 'peliculas/directores.html'
+    template_name = 'movies/directores.html'
     model = Director
 
 class DirectorView(generic.DetailView):
-    template_name = 'peliculas/director.html'
+    template_name = 'movies/director.html'
     model = Director
 
 class PeliculaView(generic.DetailView):
-    template_name = 'peliculas/pelicula.html'
+    template_name = 'movies/pelicula.html'
     model = Pelicula
