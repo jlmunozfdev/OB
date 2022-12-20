6ceb98a2f0e34eac12f8380456d5b596c2ded9d3
jlmunozfdev <jlmunozf.dev@gmail.com>
2022-12-20 14:58:00 -0500

    Sesion_2 tipos-primitivos

---
+++ JAVASCRIPT/SESION_2/tipos_primitivos.js
@@ -0,0 +1,22 @@
+/* TIPOS PRIMITIVOS EN JAVASCRIPT*/
+
+/* NUMBER */
+let number = 4;
+
+/* STRING */
+let name = 'Jorge';
+
+/* BOOLEANOS */
+let isActive = true; //true or false
+
+/* NULO Y UNDEFINED */
+let age = null;
+let sex =  undefined;
+
+/* NULL, UNDIFINED, FALSE, 0 ==> TODAS SON FALSE */
+
+if (undefined && null && false && 0) {
+    console.log('Sí cumple');
+} else {
+    console.log('No cumple');
+}
