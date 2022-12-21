8e769c90e36bfedf303e7ba7e0c9d74d88f9143f
jlmunozfdev <jlmunozf.dev@gmail.com>
2022-12-21 14:22:37 -0500

    Trabajo S5: Finished

---
+++ JAVASCRIPT/TRABAJOS/S5/ejer5_numbers.js
@@ -0,0 +1,10 @@
+let altura_centrimetros = 164;
+let altura_metros = 1.64;
+let peso_kg = 60.4;
+
+console.log(Math.ceil(altura_metros));
+console.log(Math.floor(peso_kg));
+
+let max_valor_js  = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
+console.log(max_valor_js);
