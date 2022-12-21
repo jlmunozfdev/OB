c9344319143557486fd7fa1288df4e4aff58511e
jlmunozfdev <jlmunozf.dev@gmail.com>
2022-12-21 10:49:42 -0500

    Trabajo S4: Finished

---
+++ JAVASCRIPT/TRABAJOS/S4/ejer_cadenas.js
@@ -0,0 +1,23 @@
+let nameCom = 'Jorge Luis';
+let lastName = 'Muñoz';
+let student = nameCom.concat(' ').concat(lastName);
+
+console.log(student);
+
+let studentMayus = student.toUpperCase();
+console.log(studentMayus);
+let studentMinus = student.toLowerCase();
+console.log(studentMinus);
+let studentHight = student.length;
+console.log(studentHight);
+let nameStart = student.charAt()
+console.log(nameStart);
+let lastEnd = lastName.substring(lastName.length - 1, lastName.length);
+console.log(lastEnd);
+let spaceDel = student.replace(/ /g, '');
+console.log(student);
+let nameStudent = student.includes(nameCom);
+console.log(student);
+
+
+
