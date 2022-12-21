ec17f5e603b20d2617df94649afd63e6177e4a26
jlmunozfdev <jlmunozf.dev@gmail.com>
2022-12-20 23:00:06 -0500

    Trabajo S2: Finished

---
+++ JAVASCRIPT/TRABAJOS/S2/ejer2.js
@@ -0,0 +1,15 @@
+const lista = [
+    'Jorge Luis',
+    36,
+    true,
+    new Date (1986, 09, 18),
+
+    {
+        title: 'Los Amorosos, Cartas a Chepita',
+        author: 'Jaime Sabines',
+        date: '1993',
+        url: 'https://www.libreriasur.com.pe/libro/los-amorosos-cartas-a-chepita_148727' 
+    },
+];
+
+console.log(lista);
