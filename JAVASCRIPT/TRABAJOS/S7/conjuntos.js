fc509a07919f73d0c253b87e37e6ec2be0b98772
jlmunozfdev <jlmunozf.dev@gmail.com>
2022-12-22 01:22:50 -0500

    Trabajo 7: Add conjuntos.js

---
+++ JAVASCRIPT/TRABAJOS/S7/conjuntos.js
@@ -0,0 +1,9 @@
+let setFamilia = ['Mateo', 'Ale', 'Maritza', 'Misael', 'Chris', 'Jorge'];
+
+let miSet = new Set(setFamilia);
+
+miSet.add('Jorge');
+console.log(miSet);
+
+miSet.add('Javascript');
+console.log(miSet);
